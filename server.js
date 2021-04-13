7bcf71ea60b534512bb967e89aa7c15409f348bd
KenzieAlyce <mackenziealyce@gmail.com>
2021-04-12 20:54:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-// Requiring necessary npm packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-// var db = require("./models");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({
+    extended: true
+}));
 
-// Creating express app and configuring middleware needed for authentication
-var app = express();
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(logger("dev"));
-
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
 
 mongoose.connect(
     process.env.MONGODB_URI|| "mongodb://localhost/workout",
@@ -28,12 +22,8 @@ mongoose.connect(
     }
 );
 
-// Syncing our database and logging a message to the user upon success
-app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+app.use(require('./routes/api'));
+app.use(require('./routes/index'));
+app.listen(PORT,() =>{
+    console.log(`app is running on port${PORT}`);
 });
-  
-  
-    
- 
-  
